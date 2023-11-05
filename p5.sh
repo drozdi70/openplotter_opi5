0c77b25a5905de264ff09d1bd16543c013a58e1e
Orangepi <777777@poczta.fm>
2023-11-05 20:29:02 +0100

    update8

--- p5.sh
+++ p5.sh
@@ -1,6 +1,6 @@
 sudo apt-get install gpsd gpsd-clients minicom -y
 sudo adduser orangepi dialout
-sudo echo "overlays=opi5-i2c5-m3 opi5-uart0-m2" >> /boot/armbianEnv.txt
+echo "overlays=opi5-i2c5-m3 opi5-uart0-m2" | sudo tee -a /boot/armbianEnv.txt
 sudo systemctl stop serial-getty@ttyS0.service
 sudo systemctl disable serial-getty@ttyS0.service
 sudo cp 99-gps.rules /etc/udev/rules.d
