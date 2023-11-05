c3409a9aa125ab40c17c0966374bfa035a573e8e
Orangepi <777777@poczta.fm>
2023-11-05 19:37:13 +0100

    update3

--- p4.sh
+++ p4.sh
@@ -1,7 +1,7 @@
-sudo apt-get -y install xorg lightdm xfce4 tango-icon-theme gnome-icon-theme dbus-x11 xfce4-goodies
-sudo apt-get install xfonts-base
-sudo apt-get install tightvncserver xterm
-sudo apt-get install tigervnc-tools
+sudo apt-get install xorg lightdm xfce4 tango-icon-theme gnome-icon-theme dbus-x11 xfce4-goodies -y
+sudo apt-get install xfonts-base -y
+sudo apt-get install tightvncserver xterm -y
+sudo apt-get install tigervnc-tools -y
 touch ~/.Xresources
 cp vnc_start.sh /home/orangepi/work/
 cp -f vnc_start.sh /home/orangepi/.vnc/
