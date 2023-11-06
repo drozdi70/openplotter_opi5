9ccc24e3441dc1d69808e83cd092cd33b0466d17
Orangepi <777777@poczta.fm>
2023-11-06 20:55:16 +0100

    update12

--- p9.sh
+++ p9.sh
@@ -23,6 +23,7 @@ sudo apt-get install libgl1-mesa-glx libegl1-mesa libgles2-mesa libgles2-mesa-de
 sudo apt-get install mesa-utils libgl1-mesa-glx -y
 sudo apt-get install llvm llvm-dev -y
 sudo dpkg -i ./virtualgl_3.1_arm64.deb
+cd ~/work/openplotter_opi5
 echo "Please choose 1 and Y next..."
 sudo /opt/VirtualGL/bin/vglserver_config 
 sudo reboot
