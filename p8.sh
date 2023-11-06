69c941760f7dcd00cc358ee64609c689e9246d84
Orangepi <777777@poczta.fm>
2023-11-06 17:19:53 +0100

    update9

---
+++ p8.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install -y cmake
+sudo cp -f no-sdr.conf /etc/modprobe.d/
+./1installAPI.sh
---
+++ p8A.sh
@@ -0,0 +1,14 @@
+./2buildSoapy.sh
+./4buildWidgets.sh
+sudo ldconfig
+./4buildWidgets_3.2.2.1.sh
+sudo ldconfig
+cd ~/work
+git clone https://github.com/pothosware/SoapyRTLSDR.git
+cd SoapyRTLSDR
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
+sudo ldconfig
