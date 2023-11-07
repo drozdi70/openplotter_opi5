630b49bc87bcf20ed3e77c7fb3b51d917f57ca7d
Orangepi <777777@poczta.fm>
2023-11-07 19:09:31 +0100

    update22

--- p4.sh
+++ p4.sh
@@ -1,3 +1,4 @@
+sudo apt-get install --no-install-recommends gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal gnome-core -y
 sudo apt-get install xorg lightdm xfce4 tango-icon-theme gnome-icon-theme dbus-x11 xfce4-goodies -y
 sudo apt-get install xfonts-base -y
 sudo apt-get install tightvncserver xterm -y
