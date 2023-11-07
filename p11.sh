c0bf4d255baccae888944b6cc5c19d3a4677890e
Orangepi <777777@poczta.fm>
2023-11-07 18:53:38 +0100

    update21

--- p11.sh
+++ p11.sh
@@ -7,3 +7,10 @@ cmake ..
 make -j4
 sudo make install
 sudo ldconfig
+cd ~/work/openplotter_opi5
+cp aiscatcher.conf ~/work/
+cp start-ais.sh ~/work/
+sudo cp aiscatcher.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable aiscatcher.service
+sudo systemctl start aiscatcher.service
