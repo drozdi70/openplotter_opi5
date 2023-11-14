3a3d3c7d108a61a4df702e0f87540469e40fb785
Orangepi <777777@poczta.fm>
2023-11-14 18:16:11 +0100

    Update37

--- p13.sh
+++ p13.sh
@@ -3,10 +3,12 @@ sudo apt-get upgrade -y
 sudo pip3 install --upgrade setuptools 
 sudo apt-get install -y python3-smbus python3-dev i2c-tools
 sudo adduser orangepi i2c
-sudo apt-get install -y python3 git python3-pip
+sudo apt-get install -y python3 
+sudo apt-get install python3-full -y
+sudo apt-get install git -y
+sudo apt-get install python3-pip -y
 sudo apt-get install libgpiod2 python3-libgpiod -y
 sudo pip3 install gpiod  -y
-sudo apt install python3-pip -y
 cd ~/work
 git clone https://github.com/orangepi-xunlong/wiringOP.git -b next
 cd wiringOP
@@ -14,7 +16,7 @@ sudo ./build clean
 sudo ./build
 sudo pip3 install adafruit-blinka
 sudo pip3 install --upgrade adafruit-blinka adafruit-platformdetect
-#sudo python3 -m pip install --force-reinstall adafruit-blinka
+sudo python3 -m pip install --force-reinstall adafruit-blinka
 sudo pip3 install adafruit-circuitpython-ahtx0
 sudo pip3 install adafruit-circuitpython-htu21d
 cd ~/work/openplotter_opi5
