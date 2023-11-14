82bdbfdc80cb8175d0e595b01712f050e51a21a3
Orangepi <777777@poczta.fm>
2023-11-14 20:24:35 +0100

    Update46

--- p14.sh
+++ p14.sh
@@ -9,6 +9,6 @@ sudo apt-get install debhelper dh-virtualenv -y
 #cd ..
 #sudo dpkg -i openplotter-settings_3.6.6-stable_all.deb
 #cd ~/work/openplotter_opi5/
-#sudo mv /usr/lib/python3/dist-packages/openplotterSettings/openplotterSettings.py /usr/lib/python3/dist-packages/openplotterSettings/openplotterSettings.py.orig
-#sudo cp -f openplotterSettings.py /usr/lib/python3/dist-packages/openplotterSettings/openplotterSettings.py
 sudo dpkg -i openplotter-settings_4.0.10-beta_all.deb
+sudo mv /usr/lib/python3/dist-packages/openplotterSettings/openplotterSettings.py /usr/lib/python3/dist-packages/openplotterSettings/openplotterSettings.py.orig
+sudo cp -f openplotterSettings.py /usr/lib/python3/dist-packages/openplotterSettings/openplotterSettings.py
