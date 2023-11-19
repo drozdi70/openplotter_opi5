091d70552600fbb4fed8059ef68039497b9fb840
Orangepi <777777@poczta.fm>
2023-11-19 14:04:28 +0100

    update52

---
+++ p15A.sh
@@ -0,0 +1,15 @@
+sudo cd /usr/lib/python3/dist-packages/openplotterI2c
+sudo mv gpio.py gpio.py.ORIG
+sudo mv openplotterI2c.py openplotterI2c.py.ORIG
+sudo mv openplotterI2cRead.py openplotterI2cRead.py.ORIG
+sudo mv startup.py startup.py.ORIG
+cd /home/orangepi/work/openplotter_opi5
+sudo cp -pf gpio.py /usr/lib/python3/dist-packages/openplotterI2c
+sudo cp -pf openplotterI2c.py /usr/lib/python3/dist-packages/openplotterI2c
+sudo cp -pf openplotterI2cRead.py /usr/lib/python3/dist-packages/openplotterI2c
+sudo cp -pf  startup.py /usr/lib/python3/dist-packages/openplotterI2c
+cp i2c_read.sh ~/work
+sudo cp -pf openplotter-i2c-read.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable openplotter-i2c-read.service
+sudo systemctl start openplotter-i2c-read.service
