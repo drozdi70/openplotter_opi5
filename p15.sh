825b979c837b434d27c8ebfaf28bb2560e10a792
Orangepi <777777@poczta.fm>
2023-11-20 20:37:59 +0100

    update56

--- p15.sh
+++ p15.sh
@@ -11,3 +11,4 @@ sudo dpkg-buildpackage -b -j4
 cd ..
 sudo dpkg -i openplotter-i2c_3.3.12-stable_all.deb
 sudo i2cPostInstall
+sudo cp -f orangepi5.py  /usr/local/lib/python3.11/dist-packages/adafruit_blinka/board/orangepi/orangepi5.py
